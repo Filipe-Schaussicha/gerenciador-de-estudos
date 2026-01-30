c98df37d8c378453fc72b9cdaa22fb7930363d3f
Filipe S. <filipeschausst@gmail.com>
2026-01-29 21:30:59 -0300

    Adição de comentário e de ordenação de disciplinas no componente ciclo

--- src/pages/Ciclo.tsx
+++ src/pages/Ciclo.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, type FormEvent } from "react"
+import { useState, useEffect, type FormEvent, useRef } from "react"
 import enderecoBack from "../others/VarsGlobal.tsx"
 
 interface Props{
@@ -6,23 +6,42 @@ interface Props{
 }
 
 const Ciclo = (props: Props) => {
+
+  // Estado do menu de configs
   const [configAberta, setConfigAberta] = useState(false)
+
+  // Guarda texto dos inputs
   const [nomeInput, setNomeInput] = useState('')
   const [horasInput, setHorasInput] = useState('')
+
+  // Salva lista de disciplinas vindo do backend
   const [updateValues, setUpdateValues] = useState(false)
   const [values, setValues] = useState([])
 
+  // Disciplina selecionada
+  const selecionada = useRef(-1)
+
+  // Lê a lista de disciplinas do backend
   useEffect(() => {
     fetch(`${enderecoBack}/ler_ciclo`, {credentials: 'include'}).then(res=>
-      res.json().then(json => setValues(json)).catch(()=>{alert('Erro ao carregar ciclo'); setValues([])}).finally(()=>setUpdateValues(false))
-    )
+      res.json().then(json => {
+        setValues(json.sort((a: any,b: any)=>{
+          // Ordena o array, para que as disciplinas com mais horas à fazer fiquem encima
+          if(a['objetivo'] - a['estudadas'] > b['objetivo'] - b['estudadas']){
+            return -1
+          }else{
+            return 1
+          }
+        }))
+      }
+    ).catch(()=>{alert('Erro ao carregar ciclo'); setValues([])}).finally(()=>setUpdateValues(false)))
 
     return () => {
       setValues([])
     }
   }, [updateValues])
 
-  // Funções de carregamento
+  // Função para adicionar disciplina
   function add_disciplina(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
 
@@ -36,12 +55,14 @@ const Ciclo = (props: Props) => {
     ).catch(e => alert(`Erro: ${e}`))
   }
 
+  // Função para resetar ciclo (seta horas feitas para 0)
   function reseta_ciclo() {
     fetch(`${enderecoBack}/reseta_ciclo`, {credentials: 'include'}).then(res=>
       !res.ok ? alert('Erro ao resetar ciclo') : setUpdateValues(true)
     ).catch(e=> alert(`Erro: ${e}`))
   }
 
+  // Deleta disciplinas
   function deleta_ciclo(id: number) {
     fetch(`${enderecoBack}/apaga_ciclo?id=${id}`, {credentials: 'include'}).then(res => 
       !res.ok ? alert('Erro ao apagar disciplina') : setUpdateValues(true)
@@ -50,6 +71,7 @@ const Ciclo = (props: Props) => {
 
   return (
     <section className={props.className}>
+      {/* Título */}
       <h2 className="text-4xl font-medium">Ciclo de estudos</h2>
 
       {/* Abrir configs */}
@@ -60,20 +82,26 @@ const Ciclo = (props: Props) => {
       </div> 
       :
       <div className="mt-3 rounded-xl">
+        {/* Btn fechar config */}
         <button className="hover:bg-red-300 bg-red-200 mt-3 px-3 py-1 rounded-xl font-medium block w-full" onClick={()=> setConfigAberta(false)}>
         <i className="fa-solid fa-x"></i> Fechar Configurações</button>
 
+        {/* Btn restar ciclo */}
         <button className="hover:bg-blue-300 mt-3 bg-blue-200 px-3 py-1 rounded-xl font-medium block w-full" onClick={()=>{reseta_ciclo();}}>
         <i className="fa-solid fa-rotate"></i> Resetar Ciclo</button>
 
+        {/* Form para adicionar disciplinas */}
         <div className="bg-orange-100 mt-3 rounded-xl pb-3">
           <form onSubmit={(e)=>{add_disciplina(e); setNomeInput(''); setHorasInput('');}}>  
+            {/* Btn adicionar */}
             <button type="submit" className="hover:bg-green-300 mt-3 bg-green-200 px-3 py-1 rounded-xl font-medium block w-full">
             <i className="fa-solid fa-plus"></i> Adicionar Disciplina</button>
             
             <div className="w-full mt-3">
+              {/* Disciplina */}
               <input type="text" name="nome" autoComplete="off" value={nomeInput} onChange={(e)=>setNomeInput(e.currentTarget.value)} required placeholder="Disciplina" className="bg-white rounded-xl mr-1.5 p-1.5" />
 
+              {/* Horas */}
               <input type="number" name="horas" required value={horasInput} onChange={(e)=>setHorasInput(e.currentTarget.value)} placeholder="Horas" min="0" 
               className="bg-white rounded-xl mr-1.5 p-1.5" />
             </div>
@@ -85,15 +113,19 @@ const Ciclo = (props: Props) => {
       {/* Ciclo de estudos */}
       <div>
         {values.map(item => {
+          // Escolhe fundo
           const bg = item["estudadas"] == item["objetivo"] ? 'bg-blue-100' : item["estudadas"] > item["objetivo"] ? 'bg-green-100' : 'bg-red-100'
 
-           return <div key={item["id"]} 
-          className={`mt-3 flex p-3 rounded-xl w-full ${bg}`}>
+          return <div key={item["id"]} 
+            className={`mt-3 flex p-3 rounded-xl w-full ${bg}`}>
 
+            {/* Apagar disciplina */}
             <button className="mr-1.5" onClick={()=> deleta_ciclo(item["id"])}><i className="fa-solid fa-trash" /></button>
-            <input name="ciclo" type="radio" value={item["id"]} className="mr-1.5" />
+            {/* Radio button */}
+            <input name="ciclo" type="radio" value={item["id"]} className="mr-1.5" onChange={()=>{selecionada.current = item['id'];}} />
             <label htmlFor={item["id"]}>{item["nome"]}</label>
 
+            {/* Horas estudadas e á fazer */}
             <p className="ml-auto">{item["estudadas"]}/{item["objetivo"]}</p>
           </div>
         })}
