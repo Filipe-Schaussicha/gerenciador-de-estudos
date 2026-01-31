af63382bc10e8fcdeab17f1493cc1b964c1bbcff
Filipe S. <filipeschausst@gmail.com>
2026-01-30 23:09:49 -0300

    Configurando CORS para aceitar variáveis de enviroment

--- backend/app.py
+++ backend/app.py
@@ -8,13 +8,14 @@ import sqlite3 as sql
 from datetime import datetime, timedelta
 import time
 from werkzeug.security import check_password_hash
+import os
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+CORS(app, supports_credentials=True, origins=[os.getenv("LINK_FRONT")])
 
 DB_PATH = "static/banco.db"
 
